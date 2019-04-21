d759b4677eb76e1d8de682699217200ca6b92a97
Jared Schwalbe <Jared.Schwalbe@ibm.com>
2019-04-21 15:24:30 -0500

    add form validation to the modal

--- client/src/PlayerTable/PlayerModal.jsx
+++ client/src/PlayerTable/PlayerModal.jsx
@@ -22,27 +22,74 @@ class PlayerModal extends PureComponent {
 
     this.state = {
       name: props.player ? props.player.name : '',
-      winnings: props.player ? props.player.winnings : 0,
+      winnings: props.player ? props.player.winnings : '',
       country: props.player ? props.player.country : '',
       imageUrl: props.player ? props.player.imageUrl : '',
+      invalidValues: {},
     };
+  }
+
+  handleNameChange(name) {
+    const invalidValues = Object.assign({}, this.state.invalidValues);
+    if (!name) {
+      invalidValues.name = 'Required';
+    } else {
+      delete invalidValues.name;
+    }
+
+    this.setState({
+      name,
+      invalidValues,
+    });
+  }
+
+  handleWinningsChange(winnings) {
+    const invalidValues = Object.assign({}, this.state.invalidValues);
+    if (!winnings) {
+      invalidValues.winnings = 'Required';
+    } else if (isNaN(winnings)) {
+      invalidValues.winnings = 'Invalid number';
+    } else {
+      delete invalidValues.winnings;
+    }
+
+    this.setState({
+      winnings,
+      invalidValues,
+    });
+  }
+
+  handleCountryChange(country) {
+    const invalidValues = Object.assign({}, this.state.invalidValues);
+    if (!country) {
+      invalidValues.country = 'Required';
+    } else {
+      delete invalidValues.country;
+    }
+
+    this.setState({
+      country: country ? country.code : '',
+      invalidValues,
+    });
   }
 
   submitAndClose() {
     const { onClose, onSubmit, player } = this.props;
+    const { name, winnings, country, imageUrl } = this.state;
 
-    const playerToSubmit = { ...this.state };
-    if (player) {
-      playerToSubmit.id = player.id;
-    }
-
-    onSubmit(playerToSubmit);
+    onSubmit({
+      id: player ? player.id : -1,
+      winnings: Number(winnings),
+      name,
+      country,
+      imageUrl,
+    });
     onClose();
   }
 
   render() {
     const { onClose, player } = this.props;
-    const { name, winnings, country, imageUrl } = this.state;
+    const { name, winnings, country, imageUrl, invalidValues } = this.state;
 
     return (
       <Modal
@@ -51,37 +98,46 @@ class PlayerModal extends PureComponent {
         onRequestClose={onClose}
         onRequestSubmit={this.submitAndClose}
         open={true}
+        primaryButtonDisabled={Object.keys(invalidValues).length > 0}
         primaryButtonText="Save"
         secondaryButtonText="Cancel"
       >
-        <div className="wrapper">
+        <div className="input-wrapper row">
           <TextInput
             className="player-modal__name"
             id="name"
+            invalid={Boolean(invalidValues.name)}
+            invalidText={invalidValues.name}
             labelText="Name"
-            onChange={e => this.setState({ name: e.target.value })}
+            onChange={e => this.handleNameChange(e.target.value)}
             value={name}
           />
           <TextInput
             className="player-modal__winnings"
             id="winnings"
+            invalid={Boolean(invalidValues.winnings)}
+            invalidText={invalidValues.winnings}
             labelText="Winnings"
-            onChange={e => this.setState({ winnings: e.target.value })}
+            onChange={e => this.handleWinningsChange(e.target.value)}
             value={winnings}
           />
         </div>
-        <FormLabel>Country</FormLabel>
-        <ComboBox
-          className="player-modal__country"
-          initialSelectedItem={this.countryItems.find(
-            item => item.code === country
-          )}
-          items={this.countryItems}
-          itemToString={item => (item && item.name ? item.name : '')}
-          placeholder="Filter..."
-          onChange={e => this.setState({ country: e.selectedItem.code })}
-          shouldFilterItem={e => e.item.name.includes(e.inputValue)}
-        />
+        <div className="input-wrapper">
+          <FormLabel>Country</FormLabel>
+          <ComboBox
+            className="player-modal__country"
+            initialSelectedItem={this.countryItems.find(
+              c => c.code === country
+            )}
+            invalid={Boolean(invalidValues.country)}
+            invalidText={invalidValues.country}
+            items={this.countryItems}
+            itemToString={item => (item && item.name ? item.name : '')}
+            placeholder="Filter..."
+            onChange={e => this.handleCountryChange(e.selectedItem)}
+            shouldFilterItem={e => e.item.name.includes(e.inputValue)}
+          />
+        </div>
         <TextInput
           className="player-modal__image-url"
           id="winnings"
